2811237a5bf78d5de63d62c922546e342659ad32
MartinGbz <martin.grabarz@outlook.fr>
2023-08-27 00:57:19 +0200

    feat: add mobile not supported modal

---
+++ src/app/components/mobile-modal/mobile-modal.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export const MobileModal = () => {
+  const [isDisplay, setIsDisplay] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 800) {
+      console.log("isDisplay");
+      setIsDisplay(true);
+    }
+    function resizeHandler() {
+      if (window.innerWidth < 800) {
+        setIsDisplay(true);
+      } else {
+        setIsDisplay(false);
+      }
+    }
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener("resize", resizeHandler);
+    }
+    return () => {
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener("resize", resizeHandler);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const html: any = document.getElementsByTagName("HTML")[0];
+    if (isDisplay) {
+      html.style.overflowX = "hidden";
+      html.style.overflowY = "hidden";
+    } else {
+      html.style.overflowX = "initial";
+      html.style.overflowY = "initial";
+    }
+  }, [isDisplay]);
+
+  if (!isDisplay) return;
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        zIndex: 1000,
+        width: "100vw",
+        height: "100vh",
+        background: "rgb(170 170 170 / 50%)",
+        backdropFilter: "blur(10px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <div
+        style={{
+          height: "fit-content",
+          width: "fit-content",
+          background: "white",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "rgb(0 0 0 / 25%) 0px 1px 15px 2px",
+          textAlign: "center",
+        }}>
+        <p>
+          {"Mobiles are not supported yet :("}
+          <br />
+          {"Try it on dekstop 👍"}
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { MobileModal } from "./components/mobile-modal/mobile-modal";
 import Whiteboard from "./components/whiteboard/whiteboard";
 import "./page.css";
 
 export default function Home() {
-  return <Whiteboard />;
+  return (
+    <div>
+      <MobileModal />
+      <Whiteboard />
+    </div>
+  );
 }
