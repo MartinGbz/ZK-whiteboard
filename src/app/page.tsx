ab580d840dfd646f2140417ae48c23c30baa6fbb
MartinGbz <martin.grabarz@outlook.fr>
2023-10-09 12:08:11 +0200

    fix: put whiteboard compoenent content in a page directly + put redirection to this page from the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
+"use client";
+
+import { redirect } from "next/navigation";
 import "./page.css";
 
-import WhiteboardsIndex from "../components/whiteboards-index/whiteboards-index";
-
 export default function Home() {
-  return (
-    <div>
-      <WhiteboardsIndex />
-    </div>
-  );
+  redirect("/whiteboards");
 }
